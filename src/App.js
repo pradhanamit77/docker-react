64a2ae7bfdffd15a5ad22105470d0ed100f98901
Amit Pradhan <amitpradhan@Amits-MBP.fios-router.home>
2019-12-30 20:54:09 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload(and it refreshed-with compose file!!!).
         </p>
         <a
           className="App-link"
